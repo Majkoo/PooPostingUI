a6bc19c0e8a96108ba29ec16dba531858f716ede
lasgra <tomekoclon1@gmail.com>
2024-07-19 19:10:57 +0200

    finishing touches

--- src/app/views/admin-page/admin-page.component.ts
+++ src/app/views/admin-page/admin-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, ViewChild } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToastrService } from 'ngx-toastr';
 import { catchError, tap } from 'rxjs';
