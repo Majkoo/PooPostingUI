40df8576f73703ff4e23a6805514e0a558f3c302
lasgra <tomekoclon1@gmail.com>
2024-08-29 20:03:45 +0200

    share functionality

--- src/app/shared/components/share-btn/share-btn.component.ts
+++ src/app/shared/components/share-btn/share-btn.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { DialogModule } from 'primeng/dialog';
+import {Clipboard} from '@angular/cdk/clipboard';
+import { ToastrService } from 'ngx-toastr';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'pp-share-btn',
   templateUrl: './share-btn.component.html',
+  imports: [DialogModule],
   standalone: true
 })
 export class ShareBtnComponent {
+  @Input({required: true}) imageId: string = ""
+  @Input({required: true}) creatorName: string = ""
 
+  dialogVisible: boolean = false;
+  postUrl: string = "";
+
+  private clipboard = inject(Clipboard)
+  private toastrService = inject(ToastrService)
+
+  ngOnInit(){
+    this.postUrl = `${environment.appWebUrl}/?picture=${this.imageId}`
+  }
+
+  showDialog() {
+      this.dialogVisible = true;
+  }
+
+  copyLink(){
+    this.clipboard.copy(this.postUrl)
+    this.toastrService.success("link copied");
+  }
+
+  shareTwitter(){
+    window.open(`https://twitter.com/share?text=See%20this%20poo%20post%20by%20${this.creatorName}%20&url=${this.postUrl}`, "_blank")
+  }
+  shareEmail(){
+    window.open(`mailto:?subject=See this poo post on ${environment.appWebUrl}&body=Check out this poo post ${this.postUrl}, created by ${this.creatorName} `);
+  }
+  shareTelegram(){
+    window.open(`https://t.me/share/url?url=${environment.appWebUrl}&text=Check out this poo post ${this.postUrl}, created by ${this.creatorName}!`);
+  }
+  shareFacebook(){
+    window.open(`https://www.facebook.com/sharer/sharer.php?kid_directed_site=0&sdk=joey&u=${this.postUrl}&display=popup&ref=plugin&src=share_button`);
+  }
 }
